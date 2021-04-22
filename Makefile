7376db43bd8c05d37aa36085201a284b7bed6851
brycerohrer <br269914@ohio.edu>
2021-04-22 19:58:29 -0400

    added main.cpp functions

---
+++ Makefile
@@ -0,0 +1,2 @@
+build:
+	g++ -o count main.cpp
--- main.cpp
+++ main.cpp
@@ -1,10 +1,26 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
+int countChar(string pName){
+int count = 0;
+for(int i = 0; i < pName.length(); i++){
+count = count + 1;
+}
+return count;
+}
+int countLine(string pName){
+return 0;
+}
+int main(int argc, char** argv){
 
-int main () {
+    if(argc == 1){
+        cout << "no command line" << endl;
+    }
+    else {
+        cout << "command line" << endl;
+    }
+    cout << countChar("Ohio University") << endl;
 
-
-    cout << "Hello World" << endl;
     
 }
